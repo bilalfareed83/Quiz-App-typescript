f74005b046dbb0e20dae981cfb97dd517714ef9c
bilalfareed83 <53287592+bilalfareed83@users.noreply.github.com>
2020-07-23 13:35:44 +0500

    update validation function

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, ReactEventHandler } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { getQuizData } from "./../src/services/quiz_service";
 import { QuestionType } from "./Types/quiz_types";
@@ -8,7 +8,7 @@ function App() {
   const [quiz, setQuiz] = useState<QuestionType[]>([]);
   let [currentStep, setCurrentStep] = useState(0);
   let [score, setScore] = useState(0);
-  const [selectItem, setSelectItem] = useState("");
+  let [showScore, setShowScore] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -22,32 +22,46 @@ function App() {
     return <h3>loading...</h3>;
   }
 
-  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectItem(e.target.value);
-  };
-
-  const handleSubmit = (e: React.FocusEvent<EventTarget>) => {
+  const handleSubmit = (e: React.FocusEvent<EventTarget>, userAns: String) => {
     e.preventDefault();
+    console.log(userAns);
+    let currentQuestion: QuestionType = quiz[currentStep];
+    if (userAns === currentQuestion.answer) {
+      setScore(++score);
+    }
     if (currentStep !== quiz.length - 1) {
       setCurrentStep(++currentStep);
     } else {
-      alert("Quiz completed");
       setCurrentStep(0);
-      setScore(0);
+      // setScore(0);
+      setShowScore(true);
     }
   };
-  console.log(selectItem);
+  console.log(score);
   return (
     <div className="App">
       <div className="div-quiz">
         <div className="div-header">Quiz</div>
-        <QuestionCard
-          options={quiz[currentStep].option}
-          question={quiz[currentStep].question}
-          handleSubmit={handleSubmit}
-          onChangeHandler={onChangeHandler}
-          selectItem={selectItem}
-        />
+        {showScore ? (
+          <div className="scoreCard">
+            Your score is {score}
+            <button
+              onClick={() => {
+                setScore(0);
+                setShowScore(false);
+                window.location.reload(false);
+              }}
+            >
+              Start Agian
+            </button>
+          </div>
+        ) : (
+          <QuestionCard
+            options={quiz[currentStep].option}
+            question={quiz[currentStep].question}
+            handleSubmit={handleSubmit}
+          />
+        )}
       </div>
     </div>
   );
--- quiz-app/src/Components/QuestCard.tsx
+++ quiz-app/src/Components/QuestCard.tsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
 // import Reveal from "react-reveal/Reveal";
 
-const QuestionCard: React.FC<any> = ({
-  options,
-  question,
-  handleSubmit,
-  onChangHandler,
-  selectItem,
-}) => {
+const QuestionCard: React.FC<any> = ({ options, question, handleSubmit }) => {
+  const [selectItem, setSelectItem] = useState("");
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectItem(e.target.value);
+  };
+
   return (
     <div className="div-body">
       <div className="div-question">Q. {question}</div>
       {/* <Reveal effect="fadeInUp"> */}
-      <form onSubmit={handleSubmit} className="form">
+      <form onSubmit={(e) => handleSubmit(e, selectItem)} className="form">
         {options.map((opt: string, i: number) => {
           return (
             <div key={i} className="div-option">
               <label>
-                <input type="radio" value={opt} onChange={onChangHandler} />
+                <input
+                  required
+                  type="radio"
+                  name="opt"
+                  value={opt}
+                  onChange={onChangeHandler}
+                  checked={selectItem === opt}
+                />
                 {opt}
               </label>
             </div>
